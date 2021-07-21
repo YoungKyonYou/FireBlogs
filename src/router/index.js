82b30be785863d3237cf867e30a0c655a6951e75
YoungKyonYou <man.spride1324@gmail.com>
2021-07-21 16:17:36 +0900

    feat: add routers

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Blogs from "../views/Blogs.vue";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
+import ForgotPassword from "../views/ForgotPassword.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +13,41 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+    meta: {
+      title: "Home",
+    },
+  },
+  {
+    path: "/blogs",
+    name: "Blogs",
+    component: Blogs,
+    meta: {
+      title: "Blogs",
+    },
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+    meta: {
+      title: "Login",
+    },
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register,
+    meta: {
+      title: "Register",
+    },
+  },
+  {
+    path: "/forgot-password",
+    name: "ForgotPassword",
+    component: ForgotPassword,
+    meta: {
+      title: "ForgotPassword",
+    },
   },
 ];
 
@@ -16,6 +55,16 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+});
+
+//브라우저 창 맨 위에 이름이 Home| FireBlod로 설정되게 한다.
+//beforeEach() API는 3개의 인자를 받는다
+//to: 이동할 URL 정보가 담긴 라우터 객체
+//from: 현재 URL 정보가 담긴 라우터 객체
+//next: to에서 지정한 URL로 이동하기 위해 반드시 호출 해야 하는 함수
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title} | FireBlog`;
+  next();
 });
 
 export default router;
